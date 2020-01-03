8f0deada6bc6ee209b0362b2a01b021afbe1be86
Dhruv Mohindru <dmohindru@gmail.com>
2020-01-03 08:51:57 +0530

    Solved ex 5-58. This commit marks the end of exercises for chapter 5. :-) pheww....

--- ddhw/ch5/5-58.v
+++ ddhw/ch5/5-58.v
@@ -6,7 +6,7 @@ module ex_5_58 (y_out, x_in, clk, rst);
 
     parameter S0 = 2'b00, S1 = 2'b01, S2 = 2'b10, S3 = 2'b11;
 
-    /present state logic
+    //present state logic
     always @(posedge clk, negedge rst) 
     begin
         if (!rst) state <= S0; 
@@ -18,14 +18,39 @@ module ex_5_58 (y_out, x_in, clk, rst);
         case (state)
             S0: if (x_in) begin y_out = 0; next_state = S1; end 
                 else begin y_out = 0; next_state = S0; end
-            S1: if (x_in) begin y_out = 0; next_state = S1; end 
+            S1: if (x_in) begin y_out = 0; next_state = S2; end 
                 else begin y_out = 0; next_state = S0; end
-            S2: if (x_in) begin y_out = 0; next_state = S1; end 
+            S2: if (x_in) begin y_out = 1; next_state = S3; end 
                 else begin y_out = 0; next_state = S0; end
-            S3: if (x_in) begin y_out = 0; next_state = S1; end 
+            S3: if (x_in) begin y_out = 1; next_state = S3; end 
                 else begin y_out = 0; next_state = S0; end
-
         endcase
     end
+endmodule
 
+module ex_5_58_tb;
+    reg x_in, clk, rst;
+    wire y_out;
+
+    ex_5_58 UUT(y_out, x_in, clk, rst);
+    
+    initial #200 $finish;
+    initial begin clk = 0; forever #5 clk = ~clk; end
+    initial fork
+        rst = 1;
+        x_in = 0;
+        #2 rst = 0;
+        #4 rst = 1;
+        #20 x_in = 1;
+        #60 x_in = 0;
+        #70 x_in = 1;
+        #80 x_in = 0;
+        #90 x_in = 1;
+        #100 x_in = 0;
+        #110 x_in = 1;
+        #120 x_in = 0;
+        #130 x_in = 1;
+        #160 x_in = 0;
+    join
+    initial begin $dumpfile("5-58.vcd"); $dumpvars(0, ex_5_58_tb); end
 endmodule
