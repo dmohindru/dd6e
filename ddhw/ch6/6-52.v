90c0c67f8af9a3163c053b8adc6e693bc438a7fb
Dhruv Mohindru <dmohindru@gmail.com>
2020-01-11 20:09:24 +0530

    Solved ex 6-52

--- ddhw/ch6/6-52.v
+++ ddhw/ch6/6-52.v
@@ -65,18 +65,33 @@ module ushift_4_tb;
      Sixth no change
     */
     initial fork
-        reset = 0;
-        load = 0;
-        #10 reset = 1;
-        #20 load = 1;
-        #20 i = 4'b1010;
-        #30 i = 4'b1100;
-        #40 i = 4'b0011;
-        #50 load = 0;
-        #60 load = 1;
-        #60 i = 4'b1001;
-        #70 load = 0;
-        #70 reset = 0;
+        //clear_b = 1;
+        sel = 2'b00;
+        msb_in = 1'b0;
+        lsb_in = 1'b0;
+        i_par = 4'b0000;
+        #2 clear_b = 0;
+        #3 clear_b = 1;
+        #10 sel = 2'b01; // right shitf
+        #10 msb_in = 1'b1;
+        #20 msb_in = 1'b0;
+        #30 msb_in = 1'b1;
+        #40 msb_in = 1'b0;
+        #50 sel = 2'b00; //hold value
+        #60 i_par = 4'b1101;
+        #80 sel = 2'b11; //parallel load
+        #90 sel = 2'b00; //hold value
+        #120 sel = 2'b10; //left shift
+        #120 lsb_in = 1'b1;
+        #130 lsb_in = 1'b1;
+        #140 lsb_in = 1'b0;
+        #150 lsb_in = 1'b1;
+        #160 sel = 2'b00; //hold value
+        #180 sel = 2'b11; //parallel load
+        #180 i_par = 4'b1001;
+        #190 sel = 2'b00; //hold value
+
+
     join
     initial begin $dumpfile("6-52.vcd"); $dumpvars(0, ushift_4_tb); end
 endmodule
