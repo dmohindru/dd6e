128803746251c46253b0c264fc5378576a797363
Dhruv Mohindru <dmohindru@gmail.com>
2020-02-04 00:39:16 +0530

    Solved ex 6-36

--- ddhw/ch6/6-36.v
+++ ddhw/ch6/6-36.v
@@ -31,6 +31,7 @@ module ex_6_36_str(a, up, down, clk, clear);
 
     wire down_out, a0_not, a1_not, a2_not, a3_not, t0_in, t1_in, t2_in, t3_in;
     wire w2, w3, w4, w5, w6, w7;
+    
     and(down_out, down, ~up);
 
     //input to t0
@@ -51,10 +52,34 @@ module ex_6_36_str(a, up, down, clk, clear);
     and(w7, w5, a[2]);
     or(t3_in, w6, w7);
 
-    t_ff t0(a[0], a0_not, t0_in, clk, reset);
-    t_ff t1(a[1], a1_not, t1_in, clk, reset);
-    t_ff t2(a[2], a2_not, t2_in, clk, reset);
-    t_ff t3(a[3], a3_not, t3_in, clk, reset);
+    t_ff t0(a[0], a0_not, t0_in, clk, clear);
+    t_ff t1(a[1], a1_not, t1_in, clk, clear);
+    t_ff t2(a[2], a2_not, t2_in, clk, clear);
+    t_ff t3(a[3], a3_not, t3_in, clk, clear);
 
+endmodule
+
+module ex_6_36_tb;
+    reg up, down, clk, clear;
+    wire [3:0] a_beh, a_str;
+
+    ex_6_36_beh UUT0(a_beh, up, down, clk, clear);
+    ex_6_36_str UUT1(a_str, up, down, clk, clear);
+
+    initial #300 $finish;
+    initial begin clk = 0; forever #5 clk = ~clk; end
+    initial fork
+        clear = 1;
+        up = 0;
+        down = 0;
+        #2 clear = 0;
+        #3 clear = 1;
+        #30 up = 1;
+        #100 clear = 0;
+        #102 clear = 1;
+        #130 up = 0;
+        #130 down = 1;
+    join
+    initial begin $dumpfile("6-36.vcd"); $dumpvars(0, ex_6_36_tb); end
 
 endmodule
